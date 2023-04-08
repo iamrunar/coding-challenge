8a40d1d73ace9bc878dc4dd9ddf0d2102b4ebbc3
Runar <undefvalue@gmail.com>
2023-04-08 21:16:13 +0300

    Adds next soluitions

---
+++ src/solutions/ContainsDuplicateSolver.cs
@@ -0,0 +1,19 @@
+﻿namespace solutions.easy;
+
+public class ContainsDuplicateSolver
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> hashed = new HashSet<int>(nums.Length);
+        foreach (var n in nums)
+        {
+            if (hashed.Contains(n))
+            {
+                return true;
+            }
+
+            hashed.Add(n);
+        }
+        return false;
+    }
+}
---
+++ src/solutions/ValidAnagramSolver.cs
@@ -0,0 +1,27 @@
+﻿namespace solutions.easy;
+
+public class ValidAnagramSolver
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        //int: 1 <= s.length, t.length <= 5 * 104
+        int[] hashtable = new int[26];
+
+        foreach (var c in s)
+        {
+            ++hashtable[c - 'a'];
+        }
+
+        foreach (var c in t)
+        {
+            if (--hashtable[c - 'a']<0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
