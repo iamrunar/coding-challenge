35d0d8cca1e68c6ffb8dceda2e119e371176d499
Runar <undefvalue@gmail.com>
2023-04-10 21:16:50 +0300

    Adds sorted array and Tree

---
+++ src/solutions/SortedArrayToBSTSolver.cs
@@ -0,0 +1,22 @@
+﻿namespace solutions.easy;
+
+/// <summary>
+/// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+/// Input: nums = [1,3] Output: [3,1] Explanation: [1,null,3] and[3, 1] are both height-balanced BSTs.
+/// Input: nums = [-10,-3,0,5,9] Output: [0,-3,9,-10,null,5] Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+/// </summary>
+public class SortedArrayToBSTSolver
+{
+    public TreeNode SortedArrayToBST(int[] nums)
+    {
+        return toNode(0, nums.Length-1);
+
+        TreeNode toNode(int l, int r)
+        {
+            if (l > r) { return null; }
+            var mid = (r + l) / 2;
+            return new TreeNode(nums[mid], toNode(l, mid - 1), toNode(mid + 1, r));
+        }
+    }
+}
+
---
+++ src/solutions/SquaresOfSortedArraySolver.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace solutions.easy;
+
+public class SquaresOfSortedArraySolver
+{
+    //nums in non-decreasing order
+    public int[] SortedSquares(int[] nums)
+    {
+        int[] values = new int[nums.Length];
+        for (int i = nums.Length-1, left = 0, right = nums.Length-1; i>=0;i--)
+        {
+            if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+            {
+                values[i] = nums[left] * nums[left];
+                left++;
+            }
+            else
+            {
+                values[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return values;
+    }
+
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public static class TreeNodeConverter
+{
+    public static IEnumerable<T> VisitTreeNode<T>(this TreeNode head, Func<TreeNode?, T> selector)
+    {
+        List<T> arrays = new List<T>();
+        VisitTreeNode(head, t => arrays.Add(selector(t)));
+        return arrays;
+    }
+    public static void VisitTreeNode(this TreeNode head, Action<TreeNode?> visit)
+    {
+        HashSet<TreeNode> visited = new HashSet<TreeNode>();
+        Queue<TreeNode> nodes = new Queue<TreeNode>();
+        nodes.Enqueue(head);
+
+        while (nodes.Count>0)
+        {
+            var currentNode = nodes.Dequeue();
+            if (currentNode != null)
+            {
+                if (visited.Contains(currentNode))
+                {
+                    continue;
+                }
+                visited.Add(currentNode);
+            }
+
+            visit(currentNode);
+            if (currentNode!=null && (currentNode.left!=null || currentNode.right!= null))
+            {
+                nodes.Enqueue(currentNode.left);
+                nodes.Enqueue(currentNode.right);
+            }
+        }
+    }
+}
+
