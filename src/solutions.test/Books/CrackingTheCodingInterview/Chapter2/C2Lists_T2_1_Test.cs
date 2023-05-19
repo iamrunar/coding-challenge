f4747f54a3d33430e0c2aa3616ddc9cd67830fd0
Runar <undefvalue@gmail.com>
2023-05-19 11:22:17 +0300

    Adds 2.1b-2.2abc

--- src/solutions.test/Books/CrackingTheCodingInterview/Chapter2/C2Lists_T2_1_Test.cs
+++ src/solutions.test/Books/CrackingTheCodingInterview/Chapter2/C2Lists_T2_1_Test.cs
@@ -7,12 +7,40 @@ namespace solutions.test.Books.CrackingTheCodingInterview.Chapter2
     public class C2Lists_T2_1_Test
     {
         [Theory]
-        [InlineData(new[] { 1,2,1 }, new[] { 1,2 })]
+        [InlineData(new[] { 1, 2, 1 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        [InlineData(new[] { 1, 2 }, new[] { 1, 2 })]
+        [InlineData(new[] { 1, 2, 1, 1, 3 }, new[] { 1, 2, 3 })]
+        [InlineData(new[] { 1, 2, 1, 2, 2, 2, 1, 3 }, new[] { 1, 2, 3 })]
         public void GetSet(int[] input, int[] expected)
         {
             new C2Lists_T2_1_RemoveDuplicates()
                 .RemoveDuplicates(input.ToLinkedList())
                 .ToArray()
+                .ShouldBe(expected);
+            new C2Lists_T2_1_RemoveDuplicates()
+                .RemoveDuplicates2(input.ToLinkedList())
+                .ToArray()
+                .ShouldBe(expected);
+        }
+    }
+
+    public class C2Lists_T2_2_Test
+    {
+        [Theory]
+        [InlineData(new[] { 11, 7, 5, 3 }, 1, 5)]
+        [InlineData(new[] { 11, 7, 5, 3 }, 0, 3)]
+        [InlineData(new[] { 11, 7, 5, 3 }, 3, 11)]
+        public void GetSet(int[] input, int k, int expected)
+        {
+            new C2Lists_T2_2_FindKElement()
+                .FindKElement(input.ToLinkedList(), k)
+                .ShouldBe(expected);
+            new C2Lists_T2_2_FindKElement()
+                .FindKElement2(input.ToLinkedList(), k)
+                .ShouldBe(expected);
+            new C2Lists_T2_2_FindKElement()
+                .FindKElement3(input.ToLinkedList(), k)
                 .ShouldBe(expected);
         }
     }
--- src/solutions/Books/CrackingTheCodingInterview/Chapter2/C2Lists_T2_1_RemoveDuplicates.cs
+++ src/solutions/Books/CrackingTheCodingInterview/Chapter2/C2Lists_T2_1_RemoveDuplicates.cs
@@ -6,6 +6,8 @@ namespace solutions.Books.CrackingTheCodingInterview.Chapter2
     {
         public LinkedListNode2 RemoveDuplicates(LinkedListNode2 head)
         {
+            //cpu O(n)
+            //mem O(n)
             HashSet<int> keys = new HashSet<int>();
 
             LinkedListNode2 newHead = null, newCurrent = null;
@@ -30,6 +32,80 @@ namespace solutions.Books.CrackingTheCodingInterview.Chapter2
                 current = current.next;
             }
             return newHead;
+        }
+
+        public LinkedListNode2 RemoveDuplicates2(LinkedListNode2 head)
+        {
+            //cpu O(n^2)
+            //mem O(1)
+            LinkedListNode2 current = head;
+            while (current != null)
+            {
+                LinkedListNode2 runner = current;
+                while (runner.next!=null)
+                {
+                    if (runner.next.val == current.val)
+                    {
+                        runner.next = runner.next.next;
+                    }
+                    else
+                    {
+                        runner = runner.next;
+                    }
+                }
+
+                current = current.next;
+            }
+            return head;
+        }
+    }
+
+    public class C2Lists_T2_2_FindKElement
+    {
+        public int FindKElement(LinkedListNode2 head, int k)
+        {
+            LinkedListNode2 current = head, previous = head;
+            var delta = 0;
+            while (current.next != null)
+            {
+                current = current.next;
+
+                if (delta == k) previous = previous.next;
+                else delta++;
+            }
+
+            return previous.val;
+        }
+        public int FindKElement3(LinkedListNode2 head, int k)
+        {
+            LinkedListNode2 last = head;
+            LinkedListNode2 knode = head;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (last == null) throw new InvalidOperationException("K-element not found");
+                last = last.next;
+            }
+
+            while (last.next != null)
+            {
+                last = last.next;
+                knode = knode.next;
+            }
+
+            return knode.val;
+        }
+
+        public int FindKElement2(LinkedListNode2 head, int k)
+        {
+            return FindKElement2Internal(head, k, head).val;
+
+            LinkedListNode2 FindKElement2Internal(LinkedListNode2 current, int ck, LinkedListNode2 previous)
+            {
+                if (current.next == null) return previous;
+
+                return FindKElement2Internal(current.next, ck - 1, ck <= 0 ? previous.next : previous);
+            }
         }
     }
 }
